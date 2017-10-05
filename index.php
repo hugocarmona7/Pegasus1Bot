f053a57be64f27d5b516dea4a85dce8e94fed675
hugocarmona7 <hugo.carmona@digitalcomtech.com>
2017-10-05 18:56:44 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $update = json_decode($update, TRUE);
 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-$name = $update["message"]["chat"]["first_name"];
+//$name = $update["message"]["chat"]["first_name"];
 
 switch($message)
 {
@@ -33,10 +33,10 @@ function hola($chatId)
 	enviarmensaje($chatId,$mensaje);
 }
 
-function bienvenida($chatId)
-{
+//function bienvenida($chatId)
+//{
 	//$mensaje = "Bienvenido $GLOBALS[name]";
 	//enviarmensaje($chatId,$mensaje);
-} 
+//} 
 
 ?>

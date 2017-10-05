0f2301c7b6eaea9b57533c0ae5858706b65b903f
hugocarmona7 <hugo.carmona@digitalcomtech.com>
2017-10-05 17:07:35 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $botToken = "452064424:AAFWzKkl5O2VwsjPGvtcYqZC53oY6gzwXvk";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents("php://input");
+$update = file_get_contents('php://input');
 
 $updateArray = json_decode($update, TRUE);
 

cce89611f404882a68edd69e224f932f26bd52f0
hugocarmona7 <hugo.carmona@digitalcomtech.com>
2017-10-05 20:06:35 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $update = json_decode($update, TRUE);
 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-$firstname = $update["message"]["chat"]["first_name"];
+$firstname = $update["message"]["from"]["first_name"];
 
 switch($message)
 {
